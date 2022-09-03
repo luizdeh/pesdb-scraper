52b25ca892ac82967f502686379e47393929f503
luizdeh <luizandrevr@gmail.com>
2022-09-03 10:08:03 -0300

    promise.all wrapper

--- app.js
+++ app.js
@@ -1,154 +1,217 @@
-import * as cheerio from 'cheerio';
-import fetch from 'node-fetch';
+import * as cheerio from "cheerio";
+import fetch from "node-fetch";
+
+const delay = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+const fetchPromiseUrl = async (items, url) => {
+  let results = [];
+  for (let index = 1; index <= items.length; index++) {
+    await delay();
+    const res = await fetch(`${url}${index}`);
+    results.push(res);
+  }
+  return results;
+};
 
 async function getPageNumbers() {
   try {
-	  const response = await fetch('https://pesdb.net/pes2021/');
-	  const data = await response.text();
-    const $ = cheerio.load(data)
+    const response = await fetch("https://pesdb.net/pes2021/");
+    const data = await response.text();
+    const $ = cheerio.load(data);
 
-  	let pageNumberArray = [];
-  	$('.pages > a').each((_, e) => {
+    let pageNumberArray = [];
+    $(".pages > a").each((_, e) => {
       const number = Number($(e).text());
-      console.log(number)
-  		pageNumberArray.push(number)
-  	});
-  	const max = Math.max(...pageNumberArray);
-  	pageNumberArray = [...Array(Number(max)).keys()];
-  	pageNumberArray.shift();
+      pageNumberArray.push(number);
+    });
+    const max = Math.max(...pageNumberArray);
+    pageNumberArray = [...Array(Number(max + 1)).keys()];
+    pageNumberArray.shift();
 
-  	const pageNumbersArray = [];
-  	pageNumberArray.forEach((item) => {
-  		const link = 'https://pesdb.net/pes2021/?page=';
-  		const linkNumber = String(item);
-  		pageNumbersArray.push(link.concat(linkNumber));
-  	});
-    return pageNumbersArray
-
+    const testArray = [1, 2];
+    return testArray;
+    // return pageNumberArray
   } catch (e) {
-    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`)
+    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`);
   }
 }
-// getPageNumbers()
-// getPageNumbers().then((data) => data.forEach((page) => getPlayersFromPage(page)))
 
-async function getPlayersFromPage(index) {
+async function getPlayersFromPage() {
   try {
-  const response = await fetch(`https://pesdb.net/pes2021/?page=${Number(index)}`)
-  const data = await response.text()
-  const $ = cheerio.load(data)
+    const pageNumbers = await getPageNumbers();
 
-  let playerId = []
-  $('.players tbody tr').each((i, elem) => {
-    const link = $(elem).find('td a').attr('href')
-    link 
-      ? playerId.push(link.slice(6))
-      : null
-  })
-  // console.log(playerId)
-  return playerId
+    const baseUrl = "https://pesdb.net/pes2021/?page=";
 
-  } catch(e) {
-    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`)
+    const urls = await fetchPromiseUrl(pageNumbers, baseUrl);
+
+    const tr = Promise.all(
+      urls.map(async (res) => {
+        let playersId = [];
+        const data = await res.text();
+        const $ = cheerio.load(data);
+        $(".players tbody tr").each((i, elem) => {
+          const links = $(elem).find("td a").attr("href");
+          if (links) {
+            playersId.push(links.slice(6));
+          }
+        });
+        return [...playersId];
+      })
+    );
+
+    tr.then((val) => {
+      val.map(async (arr) => {
+        const baseUrl = "https://pesdb.net/pes2021/?id=";
+        const links = await fetchPromiseUrl(arr, baseUrl);
+        const stats = Promise.all(
+          links.map(async (res) => {
+            const data = await res.text();
+            const players = getPlayerStats(data)
+            console.log(players)
+          })
+        );
+        console.log(stats);
+      });
+    });
+  } catch (e) {
+    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`);
   }
 }
 
-async function getPlayerStats(id) {
+async function test() {
+  await getPlayersFromPage();
+}
+test();
+
+async function getPlayerStats(data) {
   try {
     // fetch the player's page and load cheerio
-    const page = `https://pesdb.net/pes2021/?id=${id}`
-    const response = await fetch(page)
-    const data = await response.text()
-    const $ = cheerio.load(data)
+    // const page = `https://pesdb.net/pes2021/?id=${id}`;
+    // const response = await fetch(page);
+    // const data = await response.text();
+    const $ = cheerio.load(data);
 
     // store the cheerio values of the elements we want to traverse in
-    const table = $('.player tbody tr').first()
-    const playerInfo = table.find('td').first()
-    const playerStatsOne = playerInfo.next()
-    const playerStatsTwo = playerStatsOne.next() 
-    const playerStyles = playerStatsTwo.next()
+    const table = $(".player tbody tr").first();
+    const playerInfo = table.find("td").first();
+    const playerStatsOne = playerInfo.next();
+    const playerStatsTwo = playerStatsOne.next();
+    const playerStyles = playerStatsTwo.next();
 
     // create empty arrays to store the information we want
-    let info = []
-    let stats = []
-    let items = []
+    let info = [];
+    let stats = [];
+    let items = [];
 
     // function to get the information we need and the way we want it
     function pushStat(element, arr) {
-      let el = $(element).find('th').text().trim().slice(0,-1)
-      el = el.replace(/\s/g,'')
-      let val = $(element).find('td').text().trim()
-      el && val 
-        ? arr.push({[el]: val})
-        : null 
+      let el = $(element).find("th").text().trim().slice(0, -1);
+      el = el.replace(/\s/g, "");
+      let val = $(element).find("td").text().trim();
+      el && val ? arr.push({ [el]: val }) : null;
     }
     // get the player's information and push to the array
-    playerInfo.find('table tbody tr').each((i, elem) => pushStat(elem, info))
-    items.push(info)
+    playerInfo.find("table tbody tr").each((i, elem) => pushStat(elem, info));
+    items.push(info);
 
     // get player's playable positions and push to the info array
-    let bestPos = []
-    let goodPos = []
-    const positions = playerInfo.find('.positions div span')
+    let bestPos = [];
+    let goodPos = [];
+    const positions = playerInfo.find(".positions div span");
     positions.each((i, elem) => {
-      if ($(elem).attr('class') === 'pos2') {
-        const pos = $(elem).text().trim()
-        bestPos.push({pos})
+      if ($(elem).attr("class") === "pos2") {
+        const pos = $(elem).text().trim();
+        bestPos.push({ pos });
       }
-      if ($(elem).attr('class') === 'pos1') {
-        const pos = $(elem).text().trim()
-        goodPos.push({pos})
+      if ($(elem).attr("class") === "pos1") {
+        const pos = $(elem).text().trim();
+        goodPos.push({ pos });
       }
-    })
-    info.push({ BestPositions: bestPos })
-    info.push({ GoodPositions: goodPos })
+    });
+    info.push({ BestPositions: bestPos });
+    info.push({ GoodPositions: goodPos });
 
     // get player's playing styles and push to the array
-    let styles = []
-    playerStyles.find('table tbody tr').each((index, elem) => {
-      const item = $(elem).text()
-      styles.push({index, style: item})
-    })
-    const ids = styles.map(obj => obj.index)
-    const idToRemove = styles.findIndex((item) => item.style === 'COM Playing Styles')
-    const highestId = Math.max.apply(null,ids)
-    styles.splice(idToRemove,highestId)
-    const newStyles = styles.map(item => item.style)
-    info.push({PlayingStyles: [newStyles]})
+    let styles = [];
+    playerStyles.find("table tbody tr").each((index, elem) => {
+      const item = $(elem).text();
+      styles.push({ index, style: item });
+    });
+    const ids = styles.map((obj) => obj.index);
+    const idToRemove = styles.findIndex(
+      (item) => item.style === "COM Playing Styles"
+    );
+    const highestId = Math.max.apply(null, ids);
+    styles.splice(idToRemove, highestId);
+    const newStyles = styles.map((item) => item.style);
+    info.push({ PlayingStyles: [newStyles] });
 
     // get the player's stats and push to the array
-    playerStatsOne.find('table tbody tr').each((i, elem) => pushStat(elem, stats))
-    playerStatsTwo.find('table tbody tr').each((i, elem) => pushStat(elem, stats))
-    items.push(stats)
-  
-    // console.log(items)
+    playerStatsOne
+      .find("table tbody tr")
+      .each((i, elem) => pushStat(elem, stats));
+    playerStatsTwo
+      .find("table tbody tr")
+      .each((i, elem) => pushStat(elem, stats));
+    items.push(stats);
 
+    return items;
   } catch (e) {
-    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`)
+    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`);
   }
 }
 
 async function main() {
   try {
-    // for each page number, run getPlayersFromPage with a timeout
-   
-    const pageNumbers = await getPageNumbers()
+    const pageNumbers = await getPageNumbers();
 
-    // this works but only returns the first page
-    // await pageNumbers.reduce(async (promise, page) => {
+    let playerLinks = [];
+    async function players() {
+      try {
+        for (const page of pageNumbers) {
+          // const link = await promiseTimeout(getPlayersFromPage(page),2000)
+          // const link = await getPlayersFromPage(page)
+          // playerLinks.push(...link)
+
+          setDelay(page);
+        }
+
+        // return Promise.all(playerLinks)
+      } catch (e) {
+        console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`);
+      }
+    }
+    await players();
+
+    // async function stats(list) {
+    //   let playerStats = []
+    //   try {
+    //     for (const player of list) {
+    //       const stats = await getPlayerStats(player)
+    //       playerStats.push(stats)
+    //     }
+    //     return Promise.all(playerStats)
+    //     } catch(e) {
+    //     console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`)
+    //   }
+    // }
+
+    // const res = await pageNumbers.reduce(async (promise, page) => {
     //   await promise
-    //   const players = await getPlayersFromPage(page)
-    //   console.log(players)
+    //   await promiseTimeout(getPlayersFromPage(page),4000)
+    //   // console.log(players)
     // }, Promise.resolve())
-    //
 
-    pageNumbers.forEach(async (page) => {
-      const players = await getPlayersFromPage(page)
-      console.log(players)
-    }) 
-
-  } catch(e) {
-    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`)
+    // console.log(res)
+    // return res
+  } catch (e) {
+    console.error(`[ DEU RUIM AQUI Ó JOGADOR ] : ${e}`);
   }
 }
 // main()
+// const pages = await getPageNumbers()
+// console.log(pages)
+// const links = await getPlayersFromPage(2)
+// console.log(links)
+// const messi = await getPlayerStats(7511)
+// console.log(messi)
